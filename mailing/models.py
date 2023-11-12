cf99f6e6aa9887e77cda2532994a28b07b2be5f8
dmitryvorot <vorotnikov7009@mail.ru>
2023-11-13 03:03:18 +0800

    Add Client model

--- mailing/models.py
+++ mailing/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Client(models.Model):
+    mailings_list = models.ManyToManyField('Mailing', blank=True, related_name='clients', verbose_name='Рассылка')
+    email = models.EmailField(verbose_name='email клиента')
+    full_name = models.CharField(max_length=200, verbose_name='ФИО')
+    comment = models.TextField(max_length=300, verbose_name='Комментарий', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.full_name} - {self.email}'
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+        ordering = ('id',)
+
