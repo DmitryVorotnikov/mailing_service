756dd4bc7561602a854c0eae3f63d4cbe8496f39
dmitryvorot <vorotnikov7009@mail.ru>
2023-11-13 03:04:42 +0800

    Add Admin register

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from mailing.models import Client, Mailing, MailingLog
 
-# Register your models here.
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('email', 'full_name',)
+    search_fields = ('email', 'full_name',)
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('mailing_name', 'time', 'period', 'status', 'send_mail_subject', 'send_mail_message',)
+    list_filter = ('status',)
+    search_fields = ('mailing_name',)
+
+
+@admin.register(MailingLog)
+class MailingLogAdmin(admin.ModelAdmin):
+    list_display = ('last_mailing', 'status', 'clients_full_name', 'clients_email', 'mailing_name', 'error_message',)
+    list_filter = ('status',)
+    search_fields = ('mailing_name',)
