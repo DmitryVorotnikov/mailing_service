6f873ea67ed0a8937d379c02b3e25488e4f138f8
dmitryvorot <vorotnikov7009@mail.ru>
2023-11-13 22:45:03 +0800

    Add CRUD Client views

--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from mailing.forms import ClientForm
+from mailing.models import Client
+
+
+class ClientListView(ListView):
+    model = Client
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    form_class = ClientForm
+    success_url = reverse_lazy('mailing:clients_list')
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    form_class = ClientForm
+    success_url = reverse_lazy('mailing:clients_list')
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    success_url = reverse_lazy('mailing:clients_list')
+
+
+
