535b874c7d34654ee835818d7fe99fb884195d37
dmitryvorot <vorotnikov7009@mail.ru>
2023-11-13 22:44:53 +0800

    Add CRUD Client urls

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
 from mailing.apps import MailingConfig
+from mailing.views import ClientListView, ClientCreateView, ClientUpdateView, ClientDeleteView
 
 app_name = MailingConfig.name
 
 urlpatterns = [
-    path('', ClientListView.as_view(), name='clients'),
+    path('', ClientListView.as_view(), name='clients_list'),
+    path('create/', ClientCreateView.as_view(), name='clients_create'),
+    path('edit/<int:pk>/', ClientUpdateView.as_view(), name='clients_edit'),
+    path('delete/<int:pk>/', ClientDeleteView.as_view(), name='clients_delete'),
 ]
