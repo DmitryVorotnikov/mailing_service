4f6299691410b1d7e512e30ff73ac72d7c7920c3
dmitryvorot <vorotnikov7009@mail.ru>
2023-11-15 04:04:02 +0800

    Add send_mail views

--- mailing/views.py
+++ mailing/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from mailing.forms import ClientForm, MailingForm
 from mailing.models import Client, Mailing, MailingLog
+from mailing.services import set_mailing_affiliation, send_mail_make_report
 
 
 class ClientListView(ListView):
@@ -36,11 +36,31 @@ class MailingCreateView(CreateView):
     form_class = MailingForm
     success_url = reverse_lazy('mailing:mailings_list')
 
+    def form_valid(self, form):
+        self.object = form.save()
+
+        # Явно задаем у клиентов в поле mailings_list принадлежность к рассылке.
+        set_mailing_affiliation(self.object)
+        # Отправляем письмо и формируем отчет.
+        send_mail_make_report(self.object)
+
+        return super().form_valid(form)
+
 
 class MailingUpdateView(UpdateView):
     model = Mailing
     form_class = MailingForm
     success_url = reverse_lazy('mailing:mailings_list')
+
+    def form_valid(self, form):
+        self.object = form.save()
+
+        # Явно задаем у клиентов в поле mailings_list принадлежность к рассылке.
+        set_mailing_affiliation(self.object)
+        # Отправляем письмо и формируем отчет.
+        send_mail_make_report(self.object)
+
+        return super().form_valid(form)
 
 
 class MailingDeleteView(DeleteView):
@@ -50,4 +70,3 @@ class MailingDeleteView(DeleteView):
 
 class MailingLogListView(ListView):
     model = MailingLog
-
